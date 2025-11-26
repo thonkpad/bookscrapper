4612ce4c65254b27da03b87e275e5adbf1f6631b
thonkpad <ejanred2@protonmail.com>
2025-11-26 18:32:52 +0800

    Skeleton for API structure

--- src/main.py
+++ src/main.py
@@ -3,6 +3,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/books")
+async def get_books(
+    category: str, min_price: int, max_price: int, rating: int, sort_by: str
+):
+    return {"todo": "books"}
+
+
+@app.get("/books/{book_id}")
+async def get_book_id(book_id):
+    return {"todo": book_id}
+
+
+@app.get("/changes")
+async def get_changes():
+    return {"todo": "changes"}
+
+
 @app.get("/")
-def read_root():
+async def read_root():
     return {"hello": "world"}

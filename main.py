642c5276db7a0e2329abdd8eec3f47d4b0e75901
thonkpad <ejanred2@protonmail.com>
2025-11-26 18:08:43 +0800

    Add dev dependencies

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from bookscrapper!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"hello": "world"}

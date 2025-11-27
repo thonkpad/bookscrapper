8fb12dfbfeee1bb248fc2f0f17335357b33488c1
thonkpad <ejanred2@protonmail.com>
2025-11-27 21:37:22 +0800

    Separate logic for testing

--- src/api/routes/books.py
+++ src/api/routes/books.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-import utils.helpers as helpers
-from urllib.parse import urljoin
+from api.services.books_service import get_book_details
 
 router = APIRouter(prefix="/books", tags=["books"])
 
@@ -9,7 +8,7 @@ router = APIRouter(prefix="/books", tags=["books"])
 async def get_books(
     category: str = "all",
     min_price: float = 0.0,
-    max_price: float = 999.999,
+    max_price: float = 99.99,
     rating: int = 5,
     sort_by: str = "none",
 ):
@@ -18,36 +17,4 @@ async def get_books(
 
 @router.get("/{book_id}")
 async def get_book_id(book_id: str):
-    book = helpers.parse_book_id_html(book_id)
-
-    title = book.find("h1").text
-
-    cover = urljoin("https://books.toscrape.com", book.find("img")["src"])
-
-    category = "todo"
-
-    ratings = helpers.parse_ratings(book)
-
-    description = book.find("meta", attrs={"name": "description"}).get("content")
-
-    table = book.find("table")
-    information = {}
-
-    for row in table.find_all("tr"):
-        columns = row.find_all(["th", "td"])
-        if len(columns) == 2:
-            key = columns[0].text.strip()
-            value = columns[1].text.strip()
-
-            if "Price" in key or key == "Tax":
-                value = value.replace("Â£", "£")
-            information[key] = value
-
-    return {
-        "title": title,
-        "cover": cover,
-        "category": category,
-        "ratings": ratings,
-        "description": description,
-        "information": information,
-    }
+    return get_book_details(book_id)
---
+++ src/api/services/books_service.py
@@ -0,0 +1,34 @@
+from urllib.parse import urljoin
+from src.utils import helpers
+
+def get_book_details(book_id: str) -> dict:
+    book = helpers.parse_book_id_html(book_id)
+
+    title = book.find("h1").text
+    cover = urljoin("https://books.toscrape.com", book.find("img")["src"])
+    category = "todo"
+    ratings = helpers.parse_ratings(book)
+    description = book.find("meta", attrs={"name": "description"}).get("content")
+
+    table = book.find("table")
+    information = {}
+
+    for row in table.find_all("tr"):
+        columns = row.find_all(["th", "td"])
+        if len(columns) == 2:
+            key = columns[0].text.strip()
+            value = columns[1].text.strip()
+
+            if "Price" in key or key == "Tax":
+                value = value.replace("Â£", "£")
+
+            information[key] = value
+
+    return {
+        "title": title,
+        "cover": cover,
+        "category": category,
+        "ratings": ratings,
+        "description": description,
+        "information": information,
+    }

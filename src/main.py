8f384b23f1e96b7f44b8bc4da8df0b0e17982d64
thonkpad <ejanred2@protonmail.com>
2025-11-27 16:11:02 +0800

    Implemented basic get_book_id logic

--- src/main.py
+++ src/main.py
@@ -1,18 +1,57 @@
 from fastapi import FastAPI
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import requests
 
 app = FastAPI()
 
 
 @app.get("/books")
 async def get_books(
-    category: str, min_price: int, max_price: int, rating: int, sort_by: str
+    category: str = "all",
+    min_price: float = 0.0,
+    max_price: float = 999.999,
+    rating: int = 5,
+    sort_by: str = "none",
 ):
     return {"todo": "books"}
 
 
 @app.get("/books/{book_id}")
-async def get_book_id(book_id):
-    return {"todo": book_id}
+async def get_book_id(book_id: str):
+    url = f"https://books.toscrape.com/catalogue/{book_id}/index.html"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+
+    title = soup.find(
+        "h1"
+    ).text  # pyright: ignore[reportAny, reportOptionalMemberAccess]
+
+    cover = urljoin(
+        "https://books.toscrape.com", soup.find("img")["src"]
+    )  # pyright: ignore[reportOptionalSubscript]
+
+    description = soup.find("meta", attrs={"name": "description"}).get("content")
+
+    table = soup.find("table")
+    information = {}
+
+    for row in table.find_all("tr"):  # pyright: ignore[reportOptionalMemberAccess]
+        columns = row.find_all(["th", "td"])
+        if len(columns) == 2:
+            key = columns[0].text.strip()
+            value = columns[1].text.strip()
+
+            if "Price" in key or key == "Tax":
+                value = value.replace("Â£", "£")
+            information[key] = value
+
+    return {
+        "title": title,
+        "cover": cover,
+        "description": description,
+        "information": information,
+    }
 
 
 @app.get("/changes")

ea909809e901cc32fa33afd93cb09934789874fe
thonkpad <ejanred2@protonmail.com>
2025-11-28 13:21:22 +0800

    testing parsing category

--- src/tests/test_books_service.py
+++ src/tests/test_books_service.py
@@ -1,12 +1,12 @@
-import pytest
-from unittest.mock import patch
-from bs4 import BeautifulSoup
+from unittest.mock import MagicMock, patch
 
+import pytest
+from bs4 import BeautifulSoup
 
 EXPECTED_DATA = {
     "title": "Meditations",
     "cover": "https://books.toscrape.com/media/cache/90/f7/90f79652caecac36bc97bf7b769c8fc4.jpg",
-    "category": "todo",
+    "category": "Philosophy",
     "ratings": 2,
     "description": "\n    Written in Greek, without any intention of publication, by the only Roman emperor who was also a philosopher, the Meditations of Marcus Aurelius (AD 121-180) offer a remarkable series of challenging spiritual reflections and exercises developed as the emperor struggled to understand himself and make sense of the universe. Ranging from doubt and despair to conviction and ex Written in Greek, without any intention of publication, by the only Roman emperor who was also a philosopher, the Meditations of Marcus Aurelius (AD 121-180) offer a remarkable series of challenging spiritual reflections and exercises developed as the emperor struggled to understand himself and make sense of the universe. Ranging from doubt and despair to conviction and exaltation, they cover such diverse topics as the nature of moral virtue, human rationality, divine providence, and Marcus' own emotions. But while the Meditations were composed to provide personal consolation and encouragement, in developing his beliefs Marcus Aurelius also created one of the greatest of all works of philosophy: a timeless collection of extended meditations and short aphorisms that has been consulted and admired by statesmen, thinkers and readers through the centuries. ...more\n",
     "information": {
@@ -26,17 +26,17 @@ def mock_html():
     """Create a mock BeautifulSoup object matching the expected data"""
     html_str = f"""
     <html>
-        <h1>{EXPECTED_DATA['title']}</h1>
+        <h1>{EXPECTED_DATA["title"]}</h1>
         <img src="/media/cache/90/f7/90f79652caecac36bc97bf7b769c8fc4.jpg" />
-        <meta name="description" content="{EXPECTED_DATA['description']}" />
+        <meta name="description" content="{EXPECTED_DATA["description"]}" />
         <table>
-            <tr><th>UPC</th><td>{EXPECTED_DATA['information']['UPC']}</td></tr>
-            <tr><th>Product Type</th><td>{EXPECTED_DATA['information']['Product Type']}</td></tr>
+            <tr><th>UPC</th><td>{EXPECTED_DATA["information"]["UPC"]}</td></tr>
+            <tr><th>Product Type</th><td>{EXPECTED_DATA["information"]["Product Type"]}</td></tr>
             <tr><th>Price (excl. tax)</th><td>Â£25.89</td></tr>
             <tr><th>Price (incl. tax)</th><td>Â£25.89</td></tr>
             <tr><th>Tax</th><td>Â£0.00</td></tr>
-            <tr><th>Availability</th><td>{EXPECTED_DATA['information']['Availability']}</td></tr>
-            <tr><th>Number of reviews</th><td>{EXPECTED_DATA['information']['Number of reviews']}</td></tr>
+            <tr><th>Availability</th><td>{EXPECTED_DATA["information"]["Availability"]}</td></tr>
+            <tr><th>Number of reviews</th><td>{EXPECTED_DATA["information"]["Number of reviews"]}</td></tr>
         </table>
     </html>
     """
